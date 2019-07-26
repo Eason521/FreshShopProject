2d37cdcf2a72f3a8cb80b40673c8d2e7a63385d3
Tanyxn <1946648105@qq.com>
2019-07-26 19:31:41 +0800

    支付成功后自动跳转指定页面

--- FreshShop/Buyer/urls.py
+++ FreshShop/Buyer/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path,re_path,include
 from Buyer.views import *
 
-
 urlpatterns = [
     path("index/",index),
     path("register/",register),
     path("login/",login),
     re_path(r"goods_list/",goods_list),
+    path("pay_order/", pay_order),
+    path("pay_result/", pay_result),
 ]
 
 urlpatterns += [
