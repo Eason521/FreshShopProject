5d19e6b4ac8124bb7c6de6e495386d7bbb0ed1c1
Tanyxn <1946648105@qq.com>
2019-07-31 19:08:16 +0800

    使用api接口对列表页渲染（暂时没有分开商品状态）

--- FreshShop/FreshShop/settings.py
+++ FreshShop/FreshShop/settings.py
@@ -150,7 +150,10 @@ CKEDITOR_IMAGE_BACKEND = "pillow"
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':3
 }
+
 
 # STATIC_ROOT = os.path.join(BASE_DIR,"static")
--- FreshShop/Store/serializers.py
+++ FreshShop/Store/serializers.py
@@ -7,7 +7,8 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     """声明数据"""
     class Meta: #元类
         model = Goods  #进行接口序列化的模型
-        fields = ['goods_name','goods_price','goods_number','goods_description']
+        fields = ['goods_name','goods_price','goods_number','goods_safeDate','goods_date','goods_type_id','goods_status']
+        # fields = [i for i in Goods.objects.values()[1].keys()]
         #序列要返回的字段
 
 
